883d67e3daffc4ec7f86c2c47c1997a50d104126
21120036 <thienan2003bt@gmail.com>
2024-08-17 23:33:22 +0700

    Section 8: API Success Response classes

--- controllers/access.controller.js
+++ controllers/access.controller.js
@@ -1,13 +1,15 @@
 'use strict';
 const AccessService = require('../services/access.service');
+const { CreatedSuccessResponse } = require('../core/success.response');
 
 class AccessController {
     signUp = async (req, res, next) => {
         const data = await AccessService.signUp(req.body)
-        return res.status(201).json({
+        return new CreatedSuccessResponse({
             code: '20001',
+            statusCode: 201,
             metadata: { data },
-        })
+        }).send(res);
     }
 }
 
